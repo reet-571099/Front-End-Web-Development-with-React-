87307a18801811eb284609edf208e918b1d1bf90
reet-571099 <63258652+reet-571099@users.noreply.github.com>
2020-09-19 16:01:20 +0530

    React Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import Form from 'reactstrap/lib/Form';
 import './App.css';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 
 
 class App extends Component {
@@ -8,9 +10,11 @@ class App extends Component {
 
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
        <Main/>
       </div>
+      </BrowserRouter>
     );
   }
 }
