1dd121bc9acb18ed4de1b7319492722b1a2be6f4
reet-571099 <63258652+reet-571099@users.noreply.github.com>
2020-10-11 23:15:19 +0530

    assignment-3 & Combining Reducers

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -35,16 +35,16 @@ class Main extends Component {
     const HomePage = ()=> {
       return(
            <Home 
-              dish={this.props.state.dishes.filter((dish) => dish.featured)[0]}
-              promotion={this.props.state.promotions.filter((promo) => promo.featured)[0]}
-              leader={this.props.state.leaders.filter((leader) => leader.featured)[0]}
+              dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.props.leaders.filter((leader) => leader.featured)[0]}
            /> 
      );
     }
     const DishWithId = ({match}) => {
       return(
-          <DishDetail dish={this.props.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
-            comments={this.props.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+          <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+            comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
       );
     };
 
@@ -54,10 +54,10 @@ class Main extends Component {
         <Header/>
         <Switch>
            <Route path="/home" component={HomePage} />
-           <Route exact path="/menu" component={()=> <Menu dishes={this.props.state.dishes}/>}/>
+           <Route exact path="/menu" component={()=> <Menu dishes={this.props.dishes}/>}/>
            <Route path="/menu/:dishId" component={DishWithId}/>
            <Route exact path='/contactus' component={Contact}/>
-           <Route exact path='/aboutus' component={() => <About leaders={this.props.state.leaders}/>}/>
+           <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders}/>}/>
            <Redirect to="/home"/>
         </Switch>
         <Footer/>
