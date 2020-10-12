c6c23eb0ae5c6be8bf8cac6fe86868a952438771
reet-571099 <63258652+reet-571099@users.noreply.github.com>
2020-10-12 11:14:57 +0530

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -43,9 +43,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 
         handleSubmit(values) {
             this.toggleModal();
-            console.log("Current state is " + JSON.stringify(values));
-            alert("Current state is " + JSON.stringify(values));
-    
+            this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
         }
     
         render() {
@@ -96,7 +94,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
     }
 
 
-    function RenderComments({comments}) {
+    function RenderComments({comments, addComment, dishId}) {
         if(comments==null) {
             return(
                 <div></div>
@@ -123,7 +121,8 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
             <div className="col-12 col-md-5 m-1 text-left">
             <h4> Comments</h4>
             <ul className="list-unstyled">{comm}</ul>
-            <CommentForm></CommentForm>
+            <CommentForm dishId={dishId} addComment={addComment} />
+
             </div>
           
           );
@@ -176,7 +175,10 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
                     </div>
                     <div className="row">
                         <RenderDish dish={props.dish}/>
-                        <RenderComments comments={props.comments}/>
+                        <RenderComments comments={props.comments}
+                         addComment={props.addComment}
+                         dishId={props.dish.id}
+                        />
                     </div>
               </div>
               
@@ -185,3 +187,20 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 
 export default DishDetail;
 
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+          
