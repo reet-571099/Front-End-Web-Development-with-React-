08e2d32292ee71c967f5125d517a4fc4904f2ec0
reet-571099 <63258652+reet-571099@users.noreply.github.com>
2020-10-12 17:44:06 +0530

    React Redux Forms Revisited

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React,{Component}  from 'react';
 import { Breadcrumb, BreadcrumbItem, Button,Label,Col,Row} from'reactstrap';
 import { Link } from 'react-router-dom';
-import {Control, LocalForm , Errors} from 'react-redux-form';
+import {Control, Form , Errors} from 'react-redux-form';
 
 const required = (val) => val && val.length;
 const maxLength = (len) =>(val) =>!(val) || (val.length <= len);
@@ -25,6 +25,7 @@ class Contact extends Component {
     console.log('Current State is: ' + JSON.stringify(values));
     alert('Current State is: ' + JSON.stringify(values));
     // event.preventDefault();
+    this.props.resetFeedbackForm();
 }
 
 
@@ -100,8 +101,7 @@ class Contact extends Component {
                       <h3>Send us your Feedback</h3>
                    </div>
                     <div className="col-12 col-md-9">
-                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
-                        <Row className="form-group">
+                    <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>                        <Row className="form-group">
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
                                     <Control.text model =".firstname" id="firstname" name="firstname"
@@ -228,7 +228,7 @@ class Contact extends Component {
                                     </Button>
                                 </Col>
                             </Row>
-                        </LocalForm>
+                        </Form>
                     </div>
                </div>
 
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,20 +1,24 @@
 import  { createStore, combineReducers,applyMiddleware } from 'redux';
+import { createForms } from 'react-redux-form';
 import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Leaders } from './leaders';
 import { Promotions } from './promotions';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-
+import { InitialFeedback } from './forms';
 
 
 export const ConfigureStore = () => {
     const store = createStore(
         combineReducers({
             dishes: Dishes,
-            comments:Comments,
-            promotions:Promotions,
-            leaders: Leaders
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders,
+            ...createForms({
+                feedback: InitialFeedback
+            })
         }),
         applyMiddleware(thunk, logger)
     );
@@ -22,3 +26,4 @@ export const ConfigureStore = () => {
 
     return store;
 }
+
