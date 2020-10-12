03fe2825a81caa240f29e3896921ba48a4fd2d07
reet-571099 <63258652+reet-571099@users.noreply.github.com>
2020-10-12 16:51:38 +0530

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,7 +3,7 @@ import { Card, CardImg, CardText, CardBody,
     CardTitle, Button, Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';  
-
+import { Loading } from './LoadingComponent';
 
 
     // componentDidMount(){
@@ -150,41 +150,55 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
             }
         }
 
-        const DishDetail = (props) =>{
-
-            console.log("DishDetail Component component render is invoked");
-
-            const dish= props.dish
-        
-            if (dish==null){
-              return(
-                <div></div>
+        const DishDetail = (props) => {
+            if (props.isLoading) {
+              return (
+                <div className="container">
+                  <div className="row">
+                    <Loading />
+                  </div>
+                </div>
               );
+            } else if (props.errMess) {
+              return (
+                <div className="container">
+                  <div className="row">
+                    <h4>{props.errMess}</h4>
+                  </div>
+                </div>
+              );
+            } else if (props.dish != null) {
+              return (
+                <div className="container">
+                  <div className="row">
+                    <Breadcrumb>
+                      <BreadcrumbItem>
+                        <Link to="/menu">Menu</Link>
+                      </BreadcrumbItem>
+          
+                      <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+          
+                    <div className="col-12">
+                      <h3>{props.dish.name}</h3>
+                      <hr />
+                    </div>
+                  </div>
+          
+                  <div className="row">
+                    <RenderDish dish={props.dish} />
+                    <RenderComments
+                      comments={props.comments}
+                      postComment={props.postComment}
+                      dishId={props.dish.id}
+                    />
+                  </div>
+                </div>
+              );
+            } else {
+              return <div></div>;
             }
-            return (
-              <div className="container">
-                    <div className="row">
-                        <Breadcrumb>
-                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                        </Breadcrumb>
-                        <div className="col-12">
-                            <h3>{props.dish.name}</h3>
-                            <hr />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <RenderDish dish={props.dish}/>
-                        <RenderComments comments={props.comments}
-                         addComment={props.addComment}
-                         dishId={props.dish.id}
-                        />
-                    </div>
-              </div>
-              
-            );
-          }
-
+          };
 export default DishDetail;
 
 
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Card,CardImg,CardImgOverlay,CardTitle,Breadcrumb,BreadcrumbItem} from 'reactstrap';
 import { Link } from 'react-router-dom';
-
+import { Loading } from './LoadingComponent';
 
 
     // constructor(props) {
@@ -51,32 +51,55 @@ import { Link } from 'react-router-dom';
   
     const Menu = (props) =>{
 
-        const menu = props.dishes.map((dish) => {
+        const menu = props.dishes.dishes.map((dish) => {
             return (
               <div key={dish.id} className="col-12 col-md-5 m-1 text-left">
                <RenderMenuItem dish={dish} ></RenderMenuItem>
               </div>
             );
         });
-        console.log("Menu Component render is invoked");
-        return (
-            <div className="container">
-                <div className="row">
-              <Breadcrumb>
-                  <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                  <BreadcrumbItem active>Menu</BreadcrumbItem>
-              </Breadcrumb>
-              <div className="col-12">
-                  <h3>Menu</h3>
-                  <hr />
-              </div>
-            </div>
-                <div className="row">
-                    {menu}
-                </div>
 
-            </div>
-        );
+        console.log("Menu Component render is invoked");
+
+        if (props.dishes.isLoading) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dishes.errMess) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <h4>{props.dishes.errMess}</h4>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        else
+            return (
+                <div className="container">
+                    <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>Menu</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>Menu</h3>
+                    <hr />
+                </div>
+                </div>
+                    <div className="row">
+                        {menu}
+                    </div>
+
+                </div>
+            );
     }
         
     
